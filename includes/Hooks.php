7aaf5ccc76b96dc724da2679ed2cf5f2de359426
ubc-tuehoang <86985864+ubc-tuehoang@users.noreply.github.com>
2023-09-29 16:30:25 -0700

    Update Hooks.php

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -146,7 +146,7 @@ class Hooks {
             'CWLLogin' => $cwl_login_name,
             'CWLRole' => $ubcAffiliation,   // TODO: check if this field is used
             'CWLNickname' => $full_name,
-            'CWLMemberOf' => 'WHATEVERR',  //CWLMemberOf
+            'CWLMemberOf' => 'WHATEVERR 23Sep2023',  //CWLMemberOf
             //'CWLSaltedID' => $CWLSaltedID, // no longer needed using PUID
             'account_status' => 1   //might never be used.
         );
